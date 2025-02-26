90373e777e7bed601393cb5e80175aa3556282b8
g1dotcorp <gagan.n@s.amity.edu>
2025-02-26 10:55:56 -0700

    In supervisiion of Sth

---
+++ Folder6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int num, rev=0, rem=0;
+    printf("Enter the number");
+    scanf("%d", &num);
+    
+    while(num>0)
+    {
+        rem=num%10;
+        rev=rev*10+rem;
+        printf("%d\n",rev);
+        num=num/10;
+    }
+
+    return 0;
+}
