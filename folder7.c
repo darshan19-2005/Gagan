706d5401c50c9febad020ac1d39ba21dbb84f6e5
g1dotcorp <gagan.n@s.amity.edu>
2025-02-26 23:59:18 -0700

    CHanges made are saved

--- folder7.c
+++ folder7.c
@@ -1,16 +1,20 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
-{
-    printf("Hello World");
 
+{
+    int Length, Breadth, Perimeter, Area;
+    printf("Enter the value of Length :");
+    scanf("%d", &Length);
+    printf("Enter the value of Breadth :");
+    scanf("%d", &Breadth);
+    Perimeter=2 * (Length + Breadth);
+    printf("The perimeter of the rectangle is :%d\n ", Perimeter);
+    Area=(Length * Breadth);
+    printf("The area of the rectangle is :%d\n", Area);
+    
     return 0;
+    
+    
+    
 }
