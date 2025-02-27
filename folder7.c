0d3190523761d8aab4f0de162bad5902e01f82e7
g1dotcorp <gagan.n@s.amity.edu>
2025-02-27 10:17:07 -0700

    All mistakes were corrected

--- folder7.c
+++ folder7.c
@@ -8,10 +8,10 @@ int main()
     scanf("%d", &Length);
     printf("Enter the value of Breadth :");
     scanf("%d", &readth);
-    Perimeter=2 * (Length + lreadth);
+    Perimeter=2 * (Length + Breadth);
     printf("The perimeter of the rectangle is :%d\n ", Perimeter);
-    A4rea=(Length * lreadth);
-    printf("The area of the rectangle is :%d\n", A4rea);
+    Area=(Length * Breadth);
+    printf("The area of the rectangle is :%d\n", Area);
     
     return 0;
     
